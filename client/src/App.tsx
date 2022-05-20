b3fb4c5fd0e9b348424a1e5ba7b8009be983c615
Dmytro Zagorodny <fgmmaker@gmail.com>
2022-05-20 15:26:31 +0300

    Init client

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <header style={{ textAlign: 'center' }}>
+        <p>
+          Game of Life
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
